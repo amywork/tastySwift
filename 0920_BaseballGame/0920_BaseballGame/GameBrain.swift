d25f08c5af7f78732a11d41d656df01347b33294
younari <instar08@gmail.com>
2017-09-21 22:57:06 +0900

    baseball Class added

--- 0920_BaseballGame/0920_BaseballGame/GameBrain.swift
+++ 0920_BaseballGame/0920_BaseballGame/GameBrain.swift
@@ -7,7 +7,7 @@
 
 
 import Foundation
-class gameBrain {
+class GameBrain {
 
     // 두개의 배열을 받아서, strike / ball을 체크해서 tuple로 넘겨준다.
     func compareCheck(arr1: [Int], arr2: [Int]) -> (s: Int,b: Int) {
@@ -23,5 +23,20 @@ class gameBrain {
         }
         return (s: strike,b: ball)
     }
+ 
+    
+    
+    // 3자릿수의 랜덤수 생성 함수
+    func makeRandomList() -> [Int] {
+        var randomNumberList: [Int] = []
+        while randomNumberList.count < 3 {
+            let randomNum: Int = Int(arc4random_uniform(10))
+            if !randomNumberList.contains(randomNum) {
+                randomNumberList.append(randomNum)
+            }
+        }
+        return randomNumberList
+    }
+    
     
 }
--- 0920_BaseballGame/0920_BaseballGame/ViewController.swift
+++ 0920_BaseballGame/0920_BaseballGame/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    //Mark - 00. Property 목록
+    //Mark - 00. Property
     private var isRunning: Bool = false
     private var displayLabelList: [UILabel]?
     private var selectedNumberList: [Int]?
@@ -20,13 +20,12 @@ class ViewController: UIViewController {
     var randomNumberList: [Int]?
     var historyStr: String = ""
 
-    //Mark - 01. UILabel 정의
+    //Mark - 01. UILabel
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var displayLabel01: UILabel!
     @IBOutlet weak var displayLabel02: UILabel!
     @IBOutlet weak var displayLabel03: UILabel!
     @IBOutlet weak var historyLabel: UITextView!
-    
   
     // Mark - 02. ViewDidLoad
     override func viewDidLoad() {
@@ -34,22 +33,25 @@ class ViewController: UIViewController {
         displayLabelList = [displayLabel01,displayLabel02,displayLabel03]
     }
     
+    //게임 Brain 모델 의 instance 생성
+    var brain: GameBrain = GameBrain()
+    
     //Mark - 03. Start button Click : 시작
-    // Running, 랜덤 넘버 만들어주기, 숫자 및 라벨 초기화
     @IBAction func btnReplay(_ sender: UIButton) {
+        resetProperty()
         isRunning = true
-        randomNumberList = makeRandomList()
-        selectedNumberList = []
+        randomNumberList = brain.makeRandomList()
         historyStr = ""
         historyLabel.text = ""
         scoreLabel.text = ""
-        for label in displayLabelList! {
-            label.text = "-"
-        }
+    }
+    
+    //Mark - 04. Clear Button (숫자 지우기)
+    @IBAction func btnCancel(_ sender: UIButton) {
+        resetProperty()
     }
 
-    //Mark - 04. UIButton: btnNumber click
-    //유저가 버튼을 누르기 시작합니다.
+    //Mark - 05. UIButton: btnNumber click
     @IBAction func btnNumClick(_ sender: UIButton) {
         if isRunning {
             let selectedNum = sender.tag
@@ -63,15 +65,11 @@ class ViewController: UIViewController {
     }
     
     
-    //연산 Brain 의 instance 생성
-    var brain: gameBrain = gameBrain()
-    
-    //Mark - 05. UIButton: btnCheck, btnCancel, btnReplay
+    //Mark - 06. UIButton: btnCheck, btnCancel, btnReplay
     @IBAction func btnCheck(_ sender: UIButton) {
         if isRunning && selectedNumberList!.count == displayLabelList!.count {
-            
-            
-            // Brain에게 계산을 부탁함
+        
+            // Brain, 계산을 부탁해
             let result = brain.compareCheck(arr1: selectedNumberList!, arr2: randomNumberList!)
 
             
@@ -85,64 +83,28 @@ class ViewController: UIViewController {
                 scoreLabel.text = "3 Out!"
                 historyStr += displayLabelStr + " : 3 Out! \n"
             }else if result.s == 3 {
-                scoreLabel.text = "Right👌🏻👏🏻♥️"
-                historyStr += displayLabelStr + " Right👌🏻👏🏻♥️"
+                scoreLabel.text = "YES👌🏻👏🏻♥️"
+                historyStr += displayLabelStr + " YES👌🏻👏🏻♥️"
             }else {
                 scoreLabel.text = "S: \(result.s), B:  \(result.b)"
                 historyStr += displayLabelStr + " S: \(result.s), B:  \(result.b) \n"
             }
             
-            
-            
-//이전 코드
-//            for i in 0..<3 {
-//                if selectedNumberList?[i] == randomNumberList?[i] {
-//                    numStrike += 1
-//                }else if randomNumberList!.contains(selectedNumberList![i]) {
-//                    numBall += 1
-//                }
-//            }
-//
-//            if numStrike + numBall == 0 {
-//                scoreLabel.text = "3 Out!"
-//                historyStr += displayLabelStr + " 3 Out! \n"
-//            }else if numStrike == 3 {
-//                scoreLabel.text = "Right👌🏻👏🏻♥️"
-//                historyStr += displayLabelStr + " Right👌🏻👏🏻♥️"
-//            }else {
-//                scoreLabel.text = "S: \(numStrike), B:  \(numBall)"
-//                historyStr += displayLabelStr + " S: \(numStrike), B:  \(numBall) \n"
-//            }
         }
         
-        selectedNumberList = []
         historyLabel.text = historyStr
+        resetProperty()
+    }
+    
+
+    //초기화 Method
+    func resetProperty() {
+        selectedNumberList = []
         numStrike = 0
         numBall = 0
         for label in displayLabelList! {
             label.text = "-"
         }
     }
-    
-    //Mark - 06. UIButton: Clear (숫자 지우기)
-    @IBAction func btnCancel(_ sender: UIButton) {
-        selectedNumberList = []
-        for label in displayLabelList! {
-            label.text = "-"
-        }
-    }
-
-    //Random Method
-    func makeRandomList() -> [Int] {
-        var randomNumberList: [Int] = []
-        while randomNumberList.count < 3 {
-            let randomNum: Int = Int(arc4random_uniform(10))
-            if !randomNumberList.contains(randomNum) {
-                randomNumberList.append(randomNum)
-            }
-        }
-        return randomNumberList
-    }
 
 }
-
