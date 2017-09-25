21cb855d73465ce6f8397097b9721d9e6bbe31bc
younari <instar08@gmail.com>
2017-09-25 13:31:22 +0900

    Xcode UI file added

---
+++ 0926_XcodeUserInterface/0926_XcodeUserInterface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  0926_XcodeUserInterface
+//
+//  Created by 김기윤 on 25/09/2017.
+//  Copyright © 2017 younari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
