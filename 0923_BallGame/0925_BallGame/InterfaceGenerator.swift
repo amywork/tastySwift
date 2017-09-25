2b8f74b2085b20a4103407410b96ed0d1f69f776
younari <instar08@gmail.com>
2017-09-25 10:57:06 +0900

    class added

---
+++ 0923_BallGame/0925_BallGame/InterfaceGenerator.swift
@@ -0,0 +1,79 @@
+//
+//  Generator.swift
+//  0925_BallGame
+//
+//  Created by 김기윤 on 25/09/2017.
+//  Copyright © 2017 yunari.me. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class InterfaceGenerator {
+    var brain = GameBrain()
+    var displayLabelList: Array<UILabel> = []
+    var isRunning: Bool = false
+    var selectedNumberList: [Int] = []
+    var randomNumberList: [Int] = []
+    var historyStr: String = ""
+    var displayLabelStr: String {
+        get {
+            var str: String = ""
+            for i in 0..<3 { str += "\(selectedNumberList[i])" }
+            return str
+        }
+    }
+    
+    func play() {
+        isRunning = true
+        selectedNumberList = []
+        historyStr = ""
+        randomNumberList = brain.makeRandomList()
+        for label in displayLabelList {
+            label.text = "-"
+        }
+    }
+    
+    func delete() {
+        for label in displayLabelList {
+            label.text = "-"
+        }
+        selectedNumberList = []
+    }
+
+    func btnClick(_ sender: UIButton, scoreLB: UILabel) {
+        if isRunning {
+            let selectedNum = sender.tag
+            if selectedNumberList.count < displayLabelList.count && !selectedNumberList.contains(selectedNum) {
+                selectedNumberList.append(selectedNum)
+                let lastIndex = selectedNumberList.count - 1
+                let inputLabel = displayLabelList[lastIndex]
+                inputLabel.text = "\(selectedNum)"
+            }else if selectedNumberList.count >= displayLabelList.count {
+                scoreLB.text = "Check 버튼을 눌러주세요!"
+            }else if selectedNumberList.contains(selectedNum) {
+                scoreLB.text = "숫자 중복은 안돼요 🤡"
+            }
+        }
+    }
+    
+
+    func check(scoreLB: UILabel, historyLB: UITextView) {
+        if isRunning && selectedNumberList.count == displayLabelList.count {
+            let finalStr = brain.compareCheck(arr1: selectedNumberList, arr2: randomNumberList, inputStr: displayLabelStr)
+            scoreLB.text = finalStr.SL
+            historyStr += finalStr.HL
+            historyLB.text = historyStr
+            selectedNumberList = []
+            for label in displayLabelList {
+                label.text = "-"
+            }
+        }else if isRunning && selectedNumberList.count < displayLabelList.count {
+            scoreLB.text = "숫자 3개를 모두 선택해주세요"
+        }else if !isRunning {
+            scoreLB.text = "play 버튼을 눌러주세요"
+        }
+    }
+    
+
+}
+
--- 0923_BallGame/0925_BallGame/ViewController.swift
+++ 0923_BallGame/0925_BallGame/ViewController.swift
@@ -10,20 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Mark - 00. UI 연관 Property
-    private var isRunning: Bool = false
-    private var displayLabelList: [UILabel]?
-    private var selectedNumberList: [Int]?
-    private var randomNumberList: [Int]?
-    private var historyStr: String?
-    private var displayLabelStr: String { // [Int] -> String 변환 후 전시하기 위함, Computed property
-        get {
-            var str: String = ""
-            for i in 0..<3 { str += "\(selectedNumberList![i])" }
-            return str
-        }
-    }
-    
+
     //Mark - 01. UILabel
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var displayLabel01: UILabel!
@@ -31,66 +18,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel03: UILabel!
     @IBOutlet weak var historyLabel: UITextView!
     
+    var generator = InterfaceGenerator()
+   
     // Mark - 02. ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        displayLabelList = [displayLabel01,displayLabel02,displayLabel03] // LB 초기화
+        generator.displayLabelList = [displayLabel01,displayLabel02,displayLabel03] // LB 초기화
     }
     
     //Mark - 03. Start button : 시작
     @IBAction func btnReplay(_ sender: UIButton) {
-        isRunning = true
-        resetProperty()
-        randomNumberList = brain.makeRandomList()
+        generator.play()
         scoreLabel.text = "시작해볼까요?👻"
-        historyStr = "" // 초기화
         historyLabel.text = ""
     }
     
     //Mark - 04. Clear Button (숫자 지우기)
     @IBAction func btnCancel(_ sender: UIButton) {
-        resetProperty()
+        generator.delete()
     }
     
     //Mark - 05. UIButton: btnNum click
     @IBAction func btnNumClick(_ sender: UIButton) {
-        if isRunning {
-            let selectedNum = sender.tag
-            if selectedNumberList!.count < displayLabelList!.count && !selectedNumberList!.contains(selectedNum) {
-                selectedNumberList?.append(selectedNum)
-                let lastIndex = selectedNumberList!.count - 1
-                let inputLabel = displayLabelList![lastIndex]
-                inputLabel.text = "\(selectedNum)"
-            }else if selectedNumberList!.count >= displayLabelList!.count {
-                scoreLabel.text = "Check 버튼을 눌러주세요!"
-            }else if selectedNumberList!.contains(selectedNum) {
-                scoreLabel.text = "숫자 중복은 안돼요 🤡"
-            }
-        }
+        generator.btnClick(sender, scoreLB: scoreLabel)
     }
 
     //Mark - 06. GameBrain 모델을 통한 btnCheck 연산
-    let brain: GameBrain = GameBrain()
     @IBAction func btnCheck(_ sender: UIButton) {
-        if isRunning && selectedNumberList!.count == displayLabelList!.count {
-            let finalStr = brain.compareCheck(arr1: selectedNumberList!, arr2: randomNumberList!, inputStr: displayLabelStr)
-            scoreLabel.text = finalStr.SL
-            historyStr! += finalStr.HL
-            historyLabel.text = historyStr
-            resetProperty()
-        }else if isRunning && selectedNumberList!.count < displayLabelList!.count {
-            scoreLabel.text = "숫자 3개를 모두 고르세요"
-        }else if !isRunning {
-            scoreLabel.text = "play 버튼을 눌러주세요"
-        }
-    }
-    
-    //초기화 함수
-    func resetProperty() {
-        selectedNumberList = []
-        for label in displayLabelList! {
-            label.text = "-"
-        }
+        generator.check(scoreLB: scoreLabel, historyLB: historyLabel)
     }
     
 }
