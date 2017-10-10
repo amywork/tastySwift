a9b7f6791c723042e08a769327b84d5439bc7ac1
younari <instar08@gmail.com>
2017-10-10 10:57:43 +0900

    -

---
+++ 1011_WarmingUp/1011_WarmingUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1011_WarmingUp
+//
+//  Created by 김기윤 on 10/10/2017.
+//  Copyright © 2017 younari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
