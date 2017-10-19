f1b7115b8a7961e77ab5f221d6b1735e7923a7d5
younari <instar08@gmail.com>
2017-10-19 20:56:02 +0900

    Data Model Added

---
+++ 1020_FriendTableView/1020_FriendTableView/DataCenter/FriendDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  FriendDataModel.swift
+//  1020_FriendTableView
+//
+//  Created by 김기윤 on 19/10/2017.
+//  Copyright © 2017 younari. All rights reserved.
+//
+
+import Foundation
---
+++ 1020_FriendTableView/1020_FriendTableView/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  1020_FriendTableView
+//
+//  Created by 김기윤 on 19/10/2017.
+//  Copyright © 2017 younari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
