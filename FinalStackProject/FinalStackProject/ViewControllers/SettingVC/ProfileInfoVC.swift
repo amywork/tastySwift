5fa5ce1c1109453b35721240ef53ec7d2ebcea13
younari <instar08@gmail.com>
2017-10-24 15:14:16 +0900

    Profile Information View Controller Added

--- FinalStackProject/FinalStackProject/ViewControllers/SettingVC/ProfileInfoVC.swift
+++ FinalStackProject/FinalStackProject/ViewControllers/SettingVC/ProfileInfoVC.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
-
-class ProfileInfoVC: UIViewController {
-
+class ProfileInfoVC: UIViewController, UITableViewDataSource, UITableViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Accounts"
+        }else if section == 1 {
+            return "Toggle Subscriptions"
+        }
+        return ""
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileInfoCell", for: indexPath)
+        cell.textLabel?.text = "Table Cell Title"
+        return cell
+    }
+
+
+   
+    
 
 }
