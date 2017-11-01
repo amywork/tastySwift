3b453cdfcf0e68f183ff2fee7c5b32d1478953e4
younari <instar08@gmail.com>
2017-11-01 14:21:59 +0900

    View Switching

--- 1101_HighSwift/1101_HighSwift/ViewController.swift
+++ 1101_HighSwift/1101_HighSwift/ViewController.swift
@@ -15,18 +15,58 @@ class ViewController: UIViewController {
     // Closures can capture and store references to any constants and variables from the context in which they are defined. (maybe in heap)
     // 변수 할당 가능, 문법 단축 사용 가능, trailing 가능
     
+    @IBOutlet weak var leftView: UIView!
+    @IBOutlet weak var centerView: UIView!
+    @IBOutlet weak var rightView: UIView!
+    var tabArray = [UIView]()
+    
+    enum subView: Int {
+        case left = 1
+        case center = 2
+        case right = 3
+    }
+    
+    func swipe(index: Int) {
+        
+    }
+    
+    @IBAction func toLeft(_ sender: UIButton) {
+        leftView.isHidden = false
+        centerView.isHidden = true
+        rightView.isHidden = true
+    }
+    
+    @IBAction func toCenter(_ sender: UIButton) {
+        leftView.isHidden = true
+        centerView.isHidden = false
+        rightView.isHidden = true
+    }
+    
+    @IBAction func toRight(_ sender: UIButton) {
+        leftView.isHidden = true
+        centerView.isHidden = true
+        rightView.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        leftView.isHidden = true
+        centerView.isHidden = true
+        rightView.isHidden = true
+        
+        tabArray.append(leftView)
+        tabArray.append(centerView)
+        tabArray.append(rightView)
         
         let someArr = ["a","b","c","d","e","f","g","h","i"]
         let names = [1,2,3,4,5,6,7,8,9,10]
         let sorted = names.sorted { $0 > $1 }
         let sorted2 = someArr.sorted { (_ a, _ b) -> Bool in a < b }
-        print(sorted)
-        print(sorted2)
-        
         let incrementByTen = makeIncrementer(forIncrement: 10)()
         print(incrementByTen)
+        print(sorted)
+        print(sorted2)
     }
     
     func makeIncrementer(forIncrement amount: Int) -> () -> Int {
@@ -39,4 +79,3 @@ class ViewController: UIViewController {
     }
 
 }
-
